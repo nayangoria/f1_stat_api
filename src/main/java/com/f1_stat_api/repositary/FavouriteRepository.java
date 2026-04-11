293e18f23bfda7be429b1c65223aaf8906e0c89c
nayangoria <77434516+nayangoria@users.noreply.github.com>
2026-04-11 20:06:54 +0530

    transactional repo update

--- src/main/java/com/f1_stat_api/repositary/FavouriteRepository.java
+++ src/main/java/com/f1_stat_api/repositary/FavouriteRepository.java
@@ -2,8 +2,12 @@ package com.f1_stat_api.repositary;
 
 import com.f1_stat_api.model.Favourite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public interface FavouriteRepository extends JpaRepository<Favourite, String> {
+   @Transactional
     void deleteByDriverId(String id);
     boolean existsByDriverId(String id);
 
