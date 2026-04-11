6128426f86a084f30d19eaff2873c4c89e3c97f9
nayangoria <77434516+nayangoria@users.noreply.github.com>
2026-04-11 15:29:46 +0530

    bug fixes

--- src/main/java/com/f1_stat_api/Controller/FavouriteController.java
+++ src/main/java/com/f1_stat_api/Controller/FavouriteController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/driver/favourite")
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 public class FavouriteController {
 
     private final FavouriteRepository favouriteRepository;
