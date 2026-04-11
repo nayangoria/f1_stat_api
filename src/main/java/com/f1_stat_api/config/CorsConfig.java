2e289a80b1c31b670cfb363b0233d527224c3c06
nayangoria <77434516+nayangoria@users.noreply.github.com>
2026-04-11 22:35:35 +0530

    update to deployment

--- src/main/java/com/f1_stat_api/config/CorsConfig.java
+++ src/main/java/com/f1_stat_api/config/CorsConfig.java
@@ -1,4 +1,4 @@
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
